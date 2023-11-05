4d401a899355ed4c4ce3581b92e769a954d3039e
Jorge Alamo <jorgealamosan@gmail.com>
2023-11-05 21:59:47 +0000

    class MainFrame

--- src/main/java/software/ulpgc/kata3/swing/MainFrame.java
+++ src/main/java/software/ulpgc/kata3/swing/MainFrame.java
@@ -1,4 +1,28 @@
 package software.ulpgc.kata3.swing;
 
-public class MainFrame {
+import software.ulpgc.kata3.HistogramDisplay;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    private HistogramDisplay histogramDisplay;
+
+    public MainFrame(){
+        setTitle("Histogram");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(600,600);
+        setLocationRelativeTo(null);
+        add(createHistogramDisplay());
+    }
+
+    private Component createHistogramDisplay() {
+        JFreePanelHistogramDisplay display = new JFreePanelHistogramDisplay();
+        histogramDisplay = display;
+        return display;
+    }
+
+    public HistogramDisplay getHistogramDisplay() {
+        return histogramDisplay;
+    }
 }
