81e8eb527fa34b5fc2600aa1f2430ae2b4e28092
Jorge Alamo <jorgealamosan@gmail.com>
2023-11-05 21:57:43 +0000

    class JFreePanelHistogramDisplay

--- src/main/java/software/ulpgc/kata3/swing/JFreePanelHistogramDisplay.java
+++ src/main/java/software/ulpgc/kata3/swing/JFreePanelHistogramDisplay.java
@@ -1,4 +1,33 @@
 package software.ulpgc.kata3.swing;
 
-public class JFreePanelHistogramDisplay {
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.statistics.HistogramDataset;
+import org.jfree.data.xy.IntervalXYDataset;
+import software.ulpgc.kata3.Histogram;
+import software.ulpgc.kata3.HistogramDisplay;
+
+import javax.swing.*;
+
+public class JFreePanelHistogramDisplay extends JPanel implements HistogramDisplay {
+    @Override
+    public void show(Histogram histogram) {
+        add(new ChartPanel(chartOf(histogram)));
+    }
+
+    private JFreeChart chartOf(Histogram histogram) {
+        return ChartFactory.createHistogram(
+                histogram.title(),
+                histogram.xAxis(),
+                histogram.yAxis(),
+                dataset(histogram.data(), histogram.bins())
+        );
+    }
+
+    private IntervalXYDataset dataset(double[] data, int bins) {
+        HistogramDataset dataset = new HistogramDataset();
+        dataset.addSeries("x", data, bins);
+        return dataset;
+    }
 }
