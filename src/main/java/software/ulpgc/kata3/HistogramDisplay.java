5627070f9f5e541f70773bb5d44cd82950fd44e5
Jorge Alamo <jorgealamosan@gmail.com>
2023-11-05 21:55:20 +0000

    interface HistogramDisplay

--- src/main/java/software/ulpgc/kata3/HistogramDisplay.java
+++ src/main/java/software/ulpgc/kata3/HistogramDisplay.java
@@ -1,4 +1,5 @@
 package software.ulpgc.kata3;
 
 public interface HistogramDisplay {
+    void show(Histogram histogram);
 }
---
+++ src/main/java/software/ulpgc/kata3/swing/JFreePanelHistogramDisplay.java
@@ -0,0 +1,4 @@
+package software.ulpgc.kata3.swing;
+
+public class JFreePanelHistogramDisplay {
+}
